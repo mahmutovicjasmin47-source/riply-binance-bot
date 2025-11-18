276631051f920fbc24ec5f6ad4cc4c70778c0558
mahmutovicjasmin47-source <mahmutovicjasmin47@gmail.com>
2025-11-18 18:43:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ import Binance from 'binance-api-node';
 
 const client = Binance({
   apiKey: process.env.BINANCE_KEY,
-  apiSecret: process.env.BINANCE_SECRET,
+  apiSecret: process.env.BINANCE_SECRET
 });
 
 async function main() {
   console.log("Bot radi ✔");
-  
-  const price = await client.prices({ symbol: process.env.SYMBOL });
-  console.log("Cijena:", price);
+
+  try {
+    const price = await client.prices({ symbol: process.env.SYMBOL });
+    console.log("Cijena:", price);
+  } catch (err) {
+    console.error("Greška:", err);
+  }
 }
 
 main();
